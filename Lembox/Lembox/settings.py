f813635639536a10d269f040ac61fbdb83c4fcf3
NazarStyranec3 <nazarsturanec@gmail.com>
2026-01-22 16:31:40 +0200

    start

--- Lembox/Lembox/settings.py
+++ Lembox/Lembox/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-46+)#90z6vu)rbifu=*aur5zm3$ofg-a**!xxds_(($e!xakh+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
